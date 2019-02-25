ee1becd83ae403dd0a77542a3ecfe5d271b66d62
djc74 <davidcooper974@gmail.com>
2019-02-25 21:50:22 +0000

    Changed site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Canterbury Flower Club`,
+    description: `The website of Canterbury Flower Club`,
+    author: `Big Tent`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
