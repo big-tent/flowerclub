174e5d4a23e76a67ea207070447d1e3935d80af5
djc74 <davidcooper974@gmail.com>
2019-03-11 13:03:29 +0000

    starting contentful link

---
+++ src/components/content/eventlisting.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const eventlisting = () => <div>Hello hello</div>
+
+export default eventlisting
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,9 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import EventListing from "../components/content/eventlisting"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`Canterbury`, `Flower`, `Club`]} />
     <h1>Hello Mum and Vicky</h1>
@@ -12,8 +13,30 @@ const IndexPage = () => (
       This is not at all what it will look like - just getting Contentful
       working!
     </p>
+    {data.allContentfulEvent.edges.map(({ node }) => {
+      return <EventListing post={{ node }} />
+    })}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const indexQuery = graphql`
+  query indexQuery {
+    allContentfulEvent {
+      edges {
+        node {
+          date
+          title
+          content {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          slug
+        }
+      }
+    }
+  }
+`
