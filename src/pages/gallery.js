9789d1bc66aaedd8d34c78c74b708eb40d60ee29
djc74 <davidcooper974@gmail.com>
2019-09-11 13:06:47 +0100

    start gallery - pictures pulled in from contentful

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,14 +1,41 @@
 import React from "react"
+import { styled } from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageHeader from "../components/pageheader"
 
-const GalleryPage = () => (
+const GalleryPage = ({ data }) => (
   <Layout>
     <SEO title="Gallery" keywords={[`Canterbury`, `Flower`, `Club`]} />
-    <h1>Gallery</h1>
-    <p>Nice pictures in here</p>
+    <PageHeader pagetitle="Gallery" />
+    <ul>
+      {data.allContentfulImage.edges.map((edge, i) => (
+        <li key={i}>
+          <img src={edge.node.image.fluid.src} />
+        </li>
+      ))}
+    </ul>
   </Layout>
 )
 
 export default GalleryPage
+
+export const galleryQuery = graphql`
+  query GalleryQuery {
+    allContentfulImage {
+      edges {
+        node {
+          image {
+            description
+            fluid {
+              src
+            }
+          }
+          slug
+          event
+        }
+      }
+    }
+  }
+`
