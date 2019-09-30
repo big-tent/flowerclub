776f99f0388b952df1185dcd7f22150499521069
djc74 <davidcooper974@gmail.com>
2019-09-30 14:46:45 +0100

    adding images to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,18 +9,19 @@ import PageHeader from "../components/pageheader"
 
 import phone from "../images/phone-icon-grey.svg"
 
-const ContactsGrid = styled.section`
+const ContactsGrid = styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   grid-auto-rows: auto;
   grid-gap: 20px;
   padding: 2rem 0;
 `
 
-const ContactHolder = styled.div`
+const ContactHolder = styled.li`
   background: #fefefe;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 1rem;
+  list-style: none;
 `
 
 const NameHolder = styled.div`
@@ -26,11 +29,12 @@ const NameHolder = styled.div`
   padding-bottom: 0.5rem;
 `
 
-const MugShot = styled.div`
+const MugShot = styled(Img)`
   background: red;
   border-radius: 30px;
-  height: inherit;
+  height: 60px;
   margin-right: 1rem;
+  object-fit: cover;
   width: 60px;
 `
 
@@ -85,30 +89,24 @@ const ContactPage = ({ data }) => (
   <Layout>
     <SEO title="Contact" keywords={[`Canterbury`, `Flower`, `Club`]} />
     <PageHeader pagetitle="Contact us" />
-    <ul>
-      <ContactsGrid>
-        {data.allContentfulContactDetails.edges.map((edge, i) => (
-          <li key={i}>
-            <ContactHolder>
-              <NameHolder>
-                <MugShot />
-                <NameTitleHolder>
-                  <Name>{edge.node.name}</Name>
-                  <Title>{edge.node.title}</Title>
-                </NameTitleHolder>
-              </NameHolder>
-              <PhoneHolder href={"tel:" + edge.node.phoneNumber}>
-                <PhoneIcon src={phone} alt="phone icon" />
-                <Phone>{edge.node.phoneNumber}</Phone>
-              </PhoneHolder>
-              <Email href="mailto:diana@canterburyflowerclub.com">
-                {edge.node.email}
-              </Email>
-            </ContactHolder>
-          </li>
-        ))}
-      </ContactsGrid>
-    </ul>
+    <ContactsGrid>
+      {data.allContentfulContactDetails.edges.map((edge, i) => (
+        <ContactHolder key={i}>
+          <NameHolder>
+            <MugShot fluid={edge.node.picture.fluid} />
+            <NameTitleHolder>
+              <Name>{edge.node.name}</Name>
+              <Title>{edge.node.title}</Title>
+            </NameTitleHolder>
+          </NameHolder>
+          <PhoneHolder href={"tel:" + edge.node.phoneNumber}>
+            <PhoneIcon src={phone} alt="phone icon" />
+            <Phone>{edge.node.phoneNumber}</Phone>
+          </PhoneHolder>
+          <Email href={"mailto:" + edge.node.email}>{edge.node.email}</Email>
+        </ContactHolder>
+      ))}
+    </ContactsGrid>
   </Layout>
 )
 
@@ -123,6 +121,11 @@ export const contactQuery = graphql`
           title
           phoneNumber
           email
+          picture {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
         }
       }
     }
